19ff9c32acb04f6ba084fed8bf9cfde4926e93bd
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2024-07-28 18:36:33 -0700

    feat: add discord presence monitoring w/ Lanyard
    See: https://github.com/phineas/lanyard

---
+++ src/components/DiscordProfile/DiscordProfile.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  interface LanyardResponse {
+    success: boolean
+    data?: {
+      activities: Array<{
+        created_at: number
+        state: string
+      }>
+      active_on_discord_desktop: boolean
+      active_on_discord_mobile: boolean
+      active_on_discord_web: boolean
+      discord_status: 'dnd' | 'online' | 'idle'
+    }
+  }
+
+  /** Discord user id */
+  export let id: string
+  /** Lanyard promise - https://github.com/phineas/lanyard */
+  const promise: Promise<LanyardResponse> = fetch('https://api.lanyard.rest/v1/users/' + id)
+    .then(async (res) => await res.json())
+    .catch((err) => {
+      console.error('failed to fetch discord status!')
+    })
+</script>
+
+<a class="flex items-center" href={`discord://-/users/${id}`}>
+  <div class="relative w-36 h-36">
+    <slot />
+    {#await promise}
+      <div class="discord-status-indicator" title={'unknown'} />
+    {:then res}
+      <div
+        class:online={res.data?.discord_status === 'online'}
+        class:idle={res.data?.discord_status === 'idle'}
+        class:dnd={res.data?.discord_status === 'dnd'}
+        class="discord-status-indicator"
+        title={res.data?.discord_status ?? 'unknown'}
+      />
+    {/await}
+  </div>
+</a>
+
+<style module>
+  .dnd {
+    @apply !border-red-500 !bg-red-700;
+  }
+
+  .online {
+    @apply !border-lime-500 !bg-lime-500;
+  }
+
+  .idle {
+    @apply !border-orange-400 !bg-orange-500;
+  }
+
+  .discord-status-indicator {
+    @apply absolute w-8 h-8 bottom-1 right-1 border-8 rounded-full border-zinc-500 bg-zinc-700;
+  }
+</style>
