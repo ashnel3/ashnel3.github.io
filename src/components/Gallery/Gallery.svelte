4897e4be44a9dc5e47003bec0f5bc760586f075b
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2024-07-28 20:25:01 -0700

    feat: add gallery page

---
+++ src/components/Gallery/Gallery.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" context="module">
+  import type { ImageMetadata } from 'astro'
+
+  export interface GalleryImage {
+    alt: string
+    image: ImageMetadata
+  }
+</script>
+
+<script lang="ts">
+  export let images: GalleryImage[]
+  let active = images[0]
+</script>
+
+<div class="relative flex w-full h-full">
+  <div class="absolute flex bottom-2 w-full justify-center">
+    <div class="flex px-2 rounded-md bg-white">
+      {#each images as img}
+        <label class="px-1 py-2">
+          <input
+            bind:group={active}
+            type="radio"
+            checked={img === active}
+            name="active"
+            value={img}
+          />
+        </label>
+      {/each}
+    </div>
+  </div>
+  <div class="flex w-full h-full items-center justify-center overflow-hidden rounded-md">
+    <img class="flex-shrink-0" alt={active.alt} src={active.image.src} />
+  </div>
+</div>
